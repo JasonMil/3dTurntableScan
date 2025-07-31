04f0ee38691b99098977c8f0ad5c8157fa86a1b4
Jason Millington <Jason.Millington@cmacgroup.com>
2025-07-31 16:27:38 +0100

    made changes screen counter working bluetooth working test stepper motor next

--- 3dTurntableScan.ino
+++ 3dTurntableScan.ino
@@ -1,26 +1,18 @@
-//Written By Nikodem Bartnik - nikodembartnik.pl
-//Code for NodeMcu and 3d printed Turntable
-//Blynk IOT to add
+/*Works with lilygo T-Display  but select ESP32 Dev Module as the board*/
 
-#define STEPPER_PIN_1 D1  //9
-#define STEPPER_PIN_2 D2  //10
-#define STEPPER_PIN_3 D3  //11
-#define STEPPER_PIN_4 D4  //12
+/* board manager esp32 by Espressif Systems use version 3.3.0 dont update to a later version */
+/* Library TFT_eSPI by bodmer version 2.5.43 dont update */
 
-#define BLYNK_PRINT Serial
 
-#define BLYNK_TEMPLATE_ID "TMPL5A5B40FL7"
-#define BLYNK_TEMPLATE_NAME "display base"
 
-#define BLYNK_AUTH_TOKEN "HKWC-qwjkFajxVro6BUKteD62TvApwOJ"
+#define STEPPER_PIN_1 17  //9
+#define STEPPER_PIN_2 2  //10
+#define STEPPER_PIN_3 15  //11
+#define STEPPER_PIN_4 13  //12
 
-#include <ESP8266WiFi.h>  // Change for different WiFi module
-#include <BlynkSimpleEsp8266.h>  // Use appropriate library for your board
-
+#include <TFT_eSPI.h>       // Hardware-specific library
+#include <SPI.h>
 #include <BleKeyboard.h>
-
-BleKeyboard bleKeyboard("ESP BLE", "lilly", 100);  // Device name, manufacturer, battery level
-
 #include <Stepper.h>
 
 // 28BYJ-48 has 64 steps per revolution * 64 gear reduction = 4096 total steps per 360°
@@ -29,162 +21,88 @@ const int stepsPerRevolution = 4096;
 // 3.6 degrees corresponds to: (3.6 / 360) * 4096 = 41 steps
 const int stepSize = 41;
 
+//Photo taken counter
+int number = 0;
+
 //Start button
-const int buttonPin = 2;
+const int buttonPin = 21;
 
-Stepper stepper(stepsPerRevolution, 8, 10, 9, 11); // IN1, IN3, IN2, IN4
 
-bool hasRun = false;
+bool lastButtonState = HIGH; // Track last state (HIGH = not pressed)
 
-char ssid[] = "EE-53WRTK";
-char pass[] = "3xkMhteKpKrLDPCf";
+TFT_eSPI tft = TFT_eSPI();  // Invoke custom library
+BleKeyboard bleKeyboard("ESP BLE", "lilly", 100);  // Device name, manufacturer, battery level
 
-BlynkTimer timer;
+Stepper stepper(stepsPerRevolution, STEPPER_PIN_1, STEPPER_PIN_2, STEPPER_PIN_3, STEPPER_PIN_4);         // IN1, IN2, IN3, IN4        
 
-int step_number = 0;
-int Blynk_Start = 0;
+void setup(void) {
+  
+  pinMode(STEPPER_PIN_1, OUTPUT);
+  pinMode(STEPPER_PIN_2, OUTPUT);
+  pinMode(STEPPER_PIN_3, OUTPUT);
+  pinMode(STEPPER_PIN_4, OUTPUT);
+  
+  pinMode(buttonPin, INPUT_PULLUP);
+  
+  bleKeyboard.begin();
+  Serial.begin(115200); 
 
-void setup() {
+  tft.init();
+  tft.fillScreen(TFT_BLACK);
 
-Serial.begin(115200);
- // while the serial stream is not open, do nothin
- while (!Serial) ;
- 
+  delay(5000);
 
-Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
-//timer.setInterval(1000L, myTimerEvent);
+if (bleKeyboard.isConnected()) {
 
-pinMode(STEPPER_PIN_1, OUTPUT);
-pinMode(STEPPER_PIN_2, OUTPUT);
-pinMode(STEPPER_PIN_3, OUTPUT);
-pinMode(STEPPER_PIN_4, OUTPUT);
-
-//Start Bluetooth
-bleKeyboard.begin();
+        tft.drawString("Bluetooth OK",10,10);
+        tft.drawString("Press button to start",10,24);  
+       delay(5000);
+  
+  } else {
+  
+    tft.fillScreen(TFT_BLACK);
+    tft.drawString("Bluetooth Fail",10,10);  
+    tft.drawString("Check Phone is bluetooth enabled",10,24);  
+  }
 
 }
-
-BLYNK_WRITE(V0) {
-  
-  int buttonState = param.asInt();  // Read button state (1 = ON, 0 = OFF);
-
-  Blynk_Start = buttonState;
-
-  Serial.println("Inside Blynk write");
-  Serial.println(buttonState);
-  
-
-  }
 
 void loop() {
+
+bool buttonState = digitalRead(buttonPin); // Read current button state
+
   
-  //if  statement here for button press
+  if (buttonState != lastButtonState) {
+    if (buttonState == LOW) {
+    
+            Serial.println("Starting loop");      
+            tft.fillScreen(TFT_BLACK);  //Clear Screen
+            tft.drawString("Taking Pictures",10,24); 
+            
+              for (int i = 0; i < 360 / 3.6; i++) {
+                
+                                                      stepper.step(stepSize);
+                                                      // two pauses allows movement and photo to be taken
+                                                      delay (1000);
+
+                                                      tft.drawString(String(number),30,100,7);
+                                                      Serial.println(String(number));
+                                        
+                                                      Serial.println("Taking Photo");  
+                                                  //   bleKeyboard.print("KEY_MEDIA_VOLUME_UP");// take picture
+                                                      //bleKeyboard.write("KEY_MEDIA_VOLUME_UP");// take picture  not sure if this is the correct way to process
+                                                      
+                                                      number++;
+                                                      
+                                                      delay(1000);  // Wait 1 seconds before sending again
+
+                                                  }       
+          number = 0;
+          delay(5000);
+          tft.fillScreen(TFT_BLACK);  //Clear Screen
+          tft.drawString("Press button to start",10,24); 
   
- if (!hasRun && digitalRead(buttonPin)  == LOW )
-
- {
-      hasRun = true;
-        for (int i = 0; i < 360 / 3.6; i++) {
-          stepper.step(stepSize);
-          // two pauses allows movement and photo to be taken
-          delay (1000)
-              if (bleKeyboard.isConnected()) {
-                delay (1000);
-                Serial.println("Taking Photo");  //Up button needs to go here
-                bleKeyboard.print("Hello World!");
-                delay(1000);  // Wait 5 seconds before sending again
-            } else {
-          
-          Serial.println("Waiting for connection...");
-            }
-        }       
-        }
-        
-  //end if  button press
-  /* old blynk code
-  Blynk.run();
-
-              //  OneStep(false,1);
-
-
- OneStep(false,Blynk_Start);
- 
- //OneStep(false,0);
-
-
-  delay(2);
-*/
-
-hasRun = false; //ready for another press
-
-}
-
-
-void OneStep(bool dir,int StrtStop){  //strtstop  starts or stops the motor
-   
-
-Serial.println(StrtStop);
-Serial.println("");
-//Serial.println(dir);
+          }
   
-  
-   if(StrtStop == 0){
-
-  //  Serial.println("motor stopped");
-
-      digitalWrite(STEPPER_PIN_1, LOW);
-      digitalWrite(STEPPER_PIN_2, LOW);
-      digitalWrite(STEPPER_PIN_3, LOW);
-      digitalWrite(STEPPER_PIN_4, LOW);
-
-   }
-   else{
-             
-        //      Serial.println("motor started");
-  
-          switch(step_number){
-            case 0:
-            digitalWrite(STEPPER_PIN_1, HIGH);
-            digitalWrite(STEPPER_PIN_2, LOW);
-            digitalWrite(STEPPER_PIN_3, LOW);
-            digitalWrite(STEPPER_PIN_4, LOW);
-            break;
-            case 1:
-            digitalWrite(STEPPER_PIN_1, LOW);
-            digitalWrite(STEPPER_PIN_2, HIGH);
-            digitalWrite(STEPPER_PIN_3, LOW);
-            digitalWrite(STEPPER_PIN_4, LOW);
-            break;
-            case 2:
-            digitalWrite(STEPPER_PIN_1, LOW);
-            digitalWrite(STEPPER_PIN_2, LOW);
-            digitalWrite(STEPPER_PIN_3, HIGH);
-            digitalWrite(STEPPER_PIN_4, LOW);
-            break;
-            case 3:
-            digitalWrite(STEPPER_PIN_1, LOW);
-            digitalWrite(STEPPER_PIN_2, LOW);
-            digitalWrite(STEPPER_PIN_3, LOW);
-            digitalWrite(STEPPER_PIN_4, HIGH);
-            break;
-          } 
-        }
-       
-           
-
-   
-
-step_number++;
-  if(step_number > 3){
-    step_number = 0;
   }
-}
-
-
-// This function sends Arduino's uptime every second to Virtual Pin 2.
-void myTimerEvent()
-{
-  // You can send any value at any time.
-  // Please don't send more that 10 values per second.
-  Blynk.virtualWrite(V2, millis() / 1000);
-}
+}    
